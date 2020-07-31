e1f8ca4ae8047e4a9e10891804f47457e0726956
christopher adamson <christopheradamson98@gmail.com>
2020-07-31 11:58:40 -0600

    checkpoint 5 started

---
+++ src/components/blog.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="blog"></div>
+</template>
+
+
+<script>
+export default {
+  name: "blog",
+  data() {
+    return {};
+  },
+  computed: {},
+  methods: {},
+  components: {},
+};
+</script>
+
+
+<style scoped>
+</style>
---
+++ src/pages/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="home row">
+    Welcome to Bloggr!
+    <div class="col-12">
+      <router-link :to="{ name: 'Books' }" class="nav-link">Books</router-link>
+      <router-link :to="{ name: 'Cooking' }" class="nav-link">Cooking</router-link>
+      <router-link :to="{ name: 'Gaming' }" class="nav-link">Gaming</router-link>
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: "home",
+  data() {
+    return {};
+  },
+  mounted() {},
+  computed: {},
+  methods: {},
+  components: {},
+};
+</script>
+
+
+<style scoped>
+</style>
